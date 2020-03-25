e5076d313684eae2ae574b016b0abb8751f1038e
david <jannaultheal@gmail.com>
2020-03-25 09:09:26 -0400

    has classes and junit now.

--- BasketPrice/src/basketPrice/BasketPrice.java
+++ BasketPrice/src/basketPrice/BasketPrice.java
@@ -1,33 +1,15 @@
 package basketPrice;
 
 import java.util.*;
-
-enum item{
-    Banana,  
-    Orange,
-    Apple,
-    Lemon,
-    Peach
-};
-
-
+import basketPrice.Item;
+import basketPrice.Constants;
 
 public class BasketPrice{
 	// I looked at several flyers and I can't find prices , so I just made up these numbers
-    static Map<item, Double> prices = Map.of(
-        item.Banana, 1.5d,
-        item.Orange, 3.2d,
-        item.Apple, 1.3d,
-        item.Lemon, 4.1d,
-        item.Peach, 3.0d 
-    );
-    
-    public static double price(List<item> basket){
-        double totalPrice = 0;
-        for(item i : basket){
-            totalPrice += prices.get(i);
-        }
-        return totalPrice * 1.13; // HST is 13% 
+   
+    public static void main(String [] args) {
+    	PriceCalculator newCalc = new PriceCalculator();
+    	System.out.println(newCalc.calculatePrice(Arrays.asList(Item.Banana, Item.Orange, Item.Apple, Item.Apple))); 
     }
     
 }
